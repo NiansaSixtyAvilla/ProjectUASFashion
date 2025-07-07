9f4bb6f2a9bcabf466a9c2fa4dcedc3c90ffee79
NiansaSixtyAvilla <niansa02@gmail.com>
2025-07-07 14:29:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,16 @@
 import streamlit as st
 import numpy as np
 from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import get_file
 from tensorflow.keras.preprocessing import image
 from PIL import Image
 
-# Load model
-model = load_model("fashion_model.keras")
+# Load model dari Google Drive
+@st.cache_resource
+def load_my_model():
+    file_id = "1Pr7CzyLYQOMGWP5_4cpLjokKWQWNGOWr"  # Ganti dengan ID file Drive kamu
+    model_url = f"https://drive.google.com/uc?export=download&id={file_id}"
+    model_path = get_file("fashion_model.keras", origin=model_url)
+    return load_model(model_path)
 
-# Daftar label kelas (ubah sesuai dataset-mu)
-labels = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
-          "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
-
-# Judul
-st.title("👗👟 Fashion Classification App")
-st.write("Upload gambar fashion (28x28 pixel, grayscale) untuk diprediksi:")
-
-# Upload gambar
-uploaded_file = st.file_uploader("Pilih gambar...", type=["jpg", "png"])
-
-if uploaded_file is not None:
-    # Tampilkan gambar
-    img = Image.open(uploaded_file).convert('L')  # Convert ke grayscale
-    st.image(img, caption='Gambar yang diupload', use_column_width=True)
-
-    # Preprocessing sesuai model
-    img = img.resize((28, 28))
-    img_array = image.img_to_array(img)
-    img_array = img_array.reshape((1, 28, 28, 1)) / 255.0
-
-    # Prediksi
-    prediction = model.predict(img_array)
-    class_idx = np.argmax(prediction)
-    confidence = np.max(prediction) * 100
-
-    st.success(f"Hasil Prediksi: **{labels[class_idx]}**")
-    st.write(f"Confidence: {confidence:.2f}%")
+model = load_my_model()
